b351e08ab4f1a4a5c649bf2c7b297922efb19140
Nbav12 <nbav12@gmail.com>
2022-06-08 12:49:36 +0300

    Initial commit

---
+++ script_stackoverflow.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+    let cookiesPopup = document.getElementsByClassName("ff-sans ps-fixed z-nav-fixed ws4 sm:w-auto p32 sm:p16 bg-black-750 fc-white bar-lg b16 l16 r16 js-consent-banner")[0];
+    cookiesPopup.remove();
+}
